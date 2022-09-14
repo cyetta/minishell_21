3ff6dcc93c46fe1935c0a4c50f8af9806e9bc768
cyetta <cyetta@student.21-school.ru>
2022-09-14 22:02:31 +0300

    added function for load redirections list in prgexec

--- exec_lst_ld.c
+++ exec_lst_ld.c
@@ -6,7 +6,7 @@
 /*   By: cyetta <cyetta@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 23:42:46 by cyetta            #+#    #+#             */
-/*   Updated: 2022/09/14 00:57:58 by cyetta           ###   ########.fr       */
+/*   Updated: 2022/09/14 21:56:21 by cyetta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,36 @@ static t_prgexec	*crt_exc_elmt(t_list *t, t_mshell *data)
 }
 
 /*
+Добавляет список редиректов
+Если редирект последний или не строка записывается NULL
+т.к. записыватся указатели на строки из списка токенов, при удалении листа
+строки фришить не надо, только элементы списка.
+для here_doc анлинкается файл и фришется строка пути.
+*/
+int	exec_frdr_add(t_list **t, t_prgexec *p, t_mshell *data)
+{
+	t_token	*rd_tkn;
+	t_list	*t;
+
+	if (((t_token *)(*t)->content)->e_lxm == HERE_DOC)
+		return (f_rdrhdoc(t, p, data));
+	rd_tkn = malloc(sizeof(t_token));
+	if (!rd_tkn)
+		exit(ft_error(ERR_MALLOC));
+	rd_tkn->e_lxm = ((t_token *)(*t)->content)->e_lxm;
+	if (!(*t)->next || ((t_token *)(*t)->next->content)->e_lxm != STRINGLN)
+		rd_tkn->value = NULL;
+	else
+		rd_tkn->value = ((t_token *)(*t)->next->content)->value;
+	t = ft_lstnew(rd_tkn);
+	if (!t)
+		exit(ft_error(ERR_MALLOC));
+	ft_lstadd_back(&p->rdr_lst, t);
+	*t = (*t)->next;
+	return (ERR_OK);
+}
+
+/*
 Creates new exec element by token list from current position, open\close files \
 for redirection, change position in list for create next exec element.
 returns ERR_OK or ERR_ on error
@@ -85,9 +115,9 @@ int	new_exc_elmt(t_prgexec	**ret, t_list **t, t_mshell *data)
 	{
 		if (((t_token *)(*t)->content)->e_lxm == STRINGLN)
 			(*ret)->argv[cnt++] = ((t_token *)(*t)->content)->value;
-		else if (((t_token *)(*t)->content)->e_lxm >= PIPE && \
+		else if (((t_token *)(*t)->content)->e_lxm >= REDIR_IN && \
 	((t_token *)(*t)->content)->e_lxm <= HERE_DOC)
-			err = open_rdr(t, *ret, data);
+			err = exec_frdr_add(t, *ret, data);
 		if (err)
 			return (err);
 		*t = (*t)->next;
